dadb0e4ba668e3805a71d766b6c73786d6ea505c
Saeed Shahsavan <s.shahsavan@gmail.com>
2026-01-16 23:30:18 +0100

    add main function for observer example

--- 10-more-examples/observer/main.go
+++ 10-more-examples/observer/main.go
@@ -1,8 +1,9 @@
-package observer
+package main
 
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 type TransportEvent[T any] struct {
@@ -25,12 +26,8 @@ func (o *TransportObservable[T]) Subscribe() <-chan TransportEvent[T] {
 func (o *TransportObservable[T]) Publish(evt TransportEvent[T]) {
 
 	for _, ch := range o.subs {
-		select {
-		case ch <- evt:
-		default:
-			// observer is slow; choose drop, block, or log
-			// depending on transport system requirements
-		}
+		ch <- evt
+
 	}
 }
 
@@ -55,4 +52,27 @@ func StartObserver[T any](ctx context.Context, id int, events <-chan TransportEv
 			}
 		}
 	}()
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	obs := &TransportObservable[string]{}
+
+	// Register two observers
+	events1 := obs.Subscribe()
+	events2 := obs.Subscribe()
+
+	StartObserver(ctx, 1, events1)
+	StartObserver(ctx, 2, events2)
+
+	// Publish a few events
+	obs.Publish(TransportEvent[string]{Name: "created", Data: "order-123"})
+	obs.Publish(TransportEvent[string]{Name: "processed", Data: "order-123"})
+	obs.Publish(TransportEvent[string]{Name: "shipped", Data: "order-123"})
+
+	// Give observers time to consume before shutting down
+	time.Sleep(100 * time.Millisecond)
+	obs.Close()
 }
